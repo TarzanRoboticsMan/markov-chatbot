04537f6048d36508fb2c5e4bd7e9b59c5189590d
rl582 <ruikeliang8@gmail.com>
2021-01-13 02:30:21 -0600

    Made it into a fruitful function

--- markov-chain.py
+++ markov-chain.py
@@ -2,22 +2,31 @@ import json
 
 file = open("data/sheeran.txt")
 
-dict = {}
+def dictifyOrderOne(file):
+    """
+    Creates an order one Markov chain
 
+    Parameter file: A file
+    Precondition: file is a string
+    """
+    dict = {}
 
-for line in file: #treats each line as individual
-	words = line.split() #capitalization and punctation are "part of" the word
-	for i in range(len(words)-1): #so last word doesnt get a value
-		word = words[i]
-		if word not in dict:
-			dict[word] = {words[i+1]:1}
-		else:
-			occurences = dict[word]
-			nextWord = words[i+1]
-			if nextWord not in occurences:
-				occurences[nextWord]=1
-			else:
-				occurences[nextWord]+=1
+    for line in file: #treats each line as individual
+    	words = line.split() #capitalization and punctation are "part of" the word
+    	for i in range(len(words)-1): #so last word doesnt get a value
+    		word = words[i]
+    		if word not in dict:
+    			dict[word] = {words[i+1]:1}
+    		else:
+    			occurences = dict[word]
+    			nextWord = words[i+1]
+    			if nextWord not in occurences:
+    				occurences[nextWord]=1
+    			else:
+    				occurences[nextWord]+=1
+
+    return dict
+
 
 
 #"dog": [["hey",2],["fight",1]]
